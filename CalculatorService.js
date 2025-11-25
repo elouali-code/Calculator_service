0a684e14b24f2129361aa4de291c01690c8f8b95
AnonymousWhizzer <Anonymouswhizzer@proton.me>
2025-11-25 19:01:02 +0100

    Update CalculatorService.js

--- CalculatorService.js
+++ CalculatorService.js
@@ -11,7 +11,7 @@ const MUL_SERVICE_IP_PORT = 'http://' + service_ip + ':31003';
 const DIV_SERVICE_IP_PORT = 'http://' + service_ip + ':31004';
 
 // FastAPI metrics endpoint
-const METRICS_SERVICE = 'http://10.10.10.100:50060/measures';
+const METRICS_SERVICE = 'http://10.10.10.230:50060/measures';
 
 String.prototype.isNumeric = function () {
     return !isNaN(parseFloat(this)) && isFinite(this);
